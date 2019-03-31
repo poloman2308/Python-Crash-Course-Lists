6ebbd4d7103af84f67f561618610e855d419b8a7
Derek Acevedo <dacevedo86@yahoo.com>
2019-03-30 22:43:24 -0500

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,15 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+names = ['derek', 'daniel', 'roger']
+
+message = "Hello " + names[0].title() + ", how was your day?"
+print(message)
+
+message = "\nHello " + names[1].title() + ", how was your day?"
+print(message)
+
+message = "\nHello " + names[2].title() + ", how was your day?"
+print(message)
+
