67224193f7faba685c0d70cf20bb12a1735690d7
Derek Acevedo <dacevedo86@yahoo.com>
2019-03-30 22:53:23 -0500

    Add files via upload

---
+++ dream_cars.py
@@ -0,0 +1,15 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+cars = ['asten martin vanquish', 'chevrolet corvette', 'nissan']
+
+message = "My ultimate dream car is an " + cars[0].title()
+print(message)
+
+message = "\nMy next dream car to own would be a 1956 " + cars[1].title()
+print(message)
+
+message = "\nFinally, I would love to own a " + cars[2].title() + " GTR"
+print(message)
+
