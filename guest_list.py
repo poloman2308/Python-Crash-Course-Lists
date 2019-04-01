0a309aace0525358e59c547e38b1f28c7131c66a
Derek Acevedo <dacevedo86@yahoo.com>
2019-03-31 19:47:10 -0500

    Add files via upload

---
+++ guest_list.py
@@ -0,0 +1,10 @@
+{
+	"cmd": ["/usr/local/bin/python3", "-u", "$file"],
+}
+
+guests = ['kobe bryant', 'michael jordan', 'tom brady']
+
+print(guests[0].title() + ", please join me for dinner.")
+print("\n" + guests[1].title() + ", please join me for dinner.")
+print("\n" + guests[2].title() + ", please join me for dinner.")
+
